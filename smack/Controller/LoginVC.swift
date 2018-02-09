5fe8c6983a5d219990c0c7b1956db4ec1f77b49a
Max Huynh <maxhuynh@MAXS-MBP.gateway>
2018-02-09 14:24:56 +1100

    Section 4

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Max Huynh on 9/2/18.
+//  Copyright © 2018 Max Huynh. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    // Outlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Max Huynh on 9/2/18.
+//  Copyright © 2018 Max Huynh. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
